6a5f56540ea3b188add099f52f870cb57861935c
buckethead1986 <rspeckpfs@gmail.com>
2017-10-24 18:35:13 -0400

    added links between pages, achievements by user

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,2 +1,31 @@
 class AchievementsController < ApplicationController
+  def index
+    @achievements = Achievement.all
+  end
+
+  def show
+    @achievement = Achievement.find_by(id: params[:id])
+    @users = users_with_achievement
+  end
+
+
+  def users_with_achievement
+    @achievement = Achievement.find_by(id: params[:id])
+    achieved = []
+    User.all.collect do |user|
+      user.achievements.collect do |achievement|
+        if achievement.name == @achievement.name
+          achieved << user
+        end
+      end
+    end
+    achieved
+  end
+
+  private
+
+  def achievement_params
+    params.require(:achievement).permit(:name, :description, :img_reference)
+  end
+
 end
--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,16 @@
 class ActivitiesController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @activity = Activity.new
+    @user = User.find_by(id: params[:user_id])
+    hash = params[:activity][:name]
+    byebug
+    hash.each do |activity_id|
+      act = Activity.find_or_create_by(id: activity_id)
+      @user.activities << act
+    end
+  end
 end
