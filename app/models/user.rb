9e0119bddd94e13d90f3d1b2e0ffd44ca6d2b29a
itisjustdiego <itisjustdiego@gmail.com>
2017-10-24 10:14:39 -0400

    added show, edit, update pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :user_activities
   has_many :activities, through: :user_activities
   has_many :achievements, through: :activities
+
+  validates :name, presence: true
+  validates :name, uniqueness: true 
+
 end
