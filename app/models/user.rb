f43a77ae8cadd1aaed52fde0a9254b61a3208c01
Ramy Khorshed <RamyKhorshed@gmail.com>
2017-10-24 11:15:07 -0400

    resolved some merge issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ApplicationRecord
   has_many :activities, through: :user_activities
   has_many :achievements, through: :activities
 
-<<<<<<< HEAD
-  has_secure_password
-=======
-  validates :name, presence: true
-  validates :name, uniqueness: true 
 
->>>>>>> 99650766b65fdd562798deb208158673a1da4b86
+  has_secure_password
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
 end
