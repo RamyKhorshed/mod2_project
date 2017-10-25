061019e081bb7e46f3477c0317dd6bc5bce7c47b
buckethead1986 <rspeckpfs@gmail.com>
2017-10-25 18:12:33 -0400

    added counting and consolidation to accomplished activities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
      @activities_list = Activity.distinct.pluck(:name)
      @public_achievements = find_user.achievements
      @all_activities = find_user.activities
-
+     @grouped_activities = grouped_activities
      @activities = Activity.all
      @categories = ['Health & Fitness', 'Relationships & Well-Being', 'Intellectual', 'Purpose', 'Professional']
    end
@@ -59,5 +59,15 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def grouped_activities
+    grouped = find_user.activities.where(accomplished: true).group_by(&:name) #finds all activities for user(find_user.activities), selects only the acomplished ones (.where(accomplished: true)), and groups those with identical activities by :name
+    i = 0 #counter for while loop
+    hash = {} #empty hash to add to
+    while i < grouped.length do
+    hash[grouped.keys[i]] = grouped[grouped.keys[i]].count #key/value of :name/count of like items
+    i += 1
+    end
+    hash
+  end
 
 end
