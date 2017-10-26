8e8126651001fa8fb5c824c0f5818aab8a9e0aa6
buckethead1986 <rspeckpfs@gmail.com>
2017-10-26 13:52:56 -0400

    css issues fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,13 +22,18 @@ class UsersController < ApplicationController
 
    def show
      find_user
+     @grouped_user_fitness = grouped_user_fitness
+     @grouped_user_relationship = grouped_user_relationship
+     @grouped_user_intellectual = grouped_user_intellectual
+     @grouped_user_purpose = grouped_user_purpose
+     @grouped_user_professional = grouped_user_professional
+
      @activities_list = Activity.distinct.pluck(:name)
      @public_achievements = find_user.achievements
-     @all_activities = find_user.activities
-     @grouped_activities = grouped_activities
+     @unaccomplished_activities = find_user.activities.where(accomplished: false)
+     @grouped_accomplished_activities = grouped_activities
      @activities = Activity.new
      @categories = ['Health & Fitness', 'Relationships & Well-Being', 'Intellectual', 'Purpose', 'Professional']
-     category_points
    end
 
   def edit
@@ -45,13 +50,12 @@ class UsersController < ApplicationController
     end
   end
 
-
-
   def destroy
     find_user.destroy
     session[:user_id] = nil
     redirect_to '/login'
   end
+
 
   private
   def user_params
@@ -66,12 +70,28 @@ class UsersController < ApplicationController
     i = 0 #counter for while loop
     hash = {} #empty hash to add to
     while i < grouped.length do
-    hash[grouped.keys[i]] = grouped[grouped.keys[i]].count #key/value of :name/count of like items
+    hash[Activity.find_by(name: grouped.keys[i])] = grouped[grouped.keys[i]].count #key/value of :name/count of like items
     i += 1
     end
     hash
   end
 
+  def grouped_user_fitness
+    grouped_activities.select {|activity| activity.category == "Health & Fitness"}
+  end
+  def grouped_user_relationship
+    grouped_activities.select {|activity| activity.category == "Relationships & Well-Being"}
+  end
+  def grouped_user_intellectual
+    grouped_activities.select {|activity| activity.category == "Intellectual"}
+  end
+  def grouped_user_purpose
+    grouped_activities.select {|activity| activity.category == "Purpose"}
+  end
+  def grouped_user_professional
+    grouped_activities.select {|activity| activity.category == "Professional"}
+  end
+  
   def category_points
     grouped = find_user.activities.where(accomplished: true).group_by(&:category) #finds all activities for user(find_user.activities), selects only the acomplished ones (.where(accomplished: true)), and groups those with identical activities by :name
     scores = {}
@@ -86,7 +106,5 @@ class UsersController < ApplicationController
     end
     @scores_array = scores.to_a
   end
-
-
 
 end
